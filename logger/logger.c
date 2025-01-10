49d2a28a1933fc438a47d74bf6d9b400a799309e
Thomas Woltering <wart3k@web.de>
2025-01-11 00:12:40 +0100

    reformat functions

--- logger/logger.c
+++ logger/logger.c
@@ -31,9 +31,6 @@ static const char LOG_INFO_CHAR[]       = "[INFO    ]: ";
 static const char LOG_DEBUG_CHAR[]      = "[DEBUG   ]: ";
 static const char LOG_TRACE_CHAR[]      = "[TRACE   ]: ";
 
-static cool_status_t log_generic(log_level_list_t level, const char * restrict func, const char * restrict msg, va_list args);
-static cool_status_t print_log_msg(const log_level_and_string_t *log_config, const char * restrict func, const char * restrict msg, va_list args);
-
 static logger_config_t logger_conf = {
     .level = INFO,
     .is_timer_available = false
@@ -47,6 +44,45 @@ static const log_level_and_string_t log_level_and_string[LOG_LEVEL_COUNT] = {
     { .level = DEBUG, .level_str = LOG_DEBUG_CHAR },
     { .level = TRACE, .level_str = LOG_TRACE_CHAR }
 };
+
+static cool_status_t print_log_msg(const log_level_and_string_t *log_config, const char * restrict func, const char * restrict msg, va_list args)
+{
+    if (log_config == NULL || logger_conf.level < log_config->level) {
+        return COOL_NULL_POINTER_ERROR;
+    }
+
+    char buffer[LOG_PRINT_BUFFER_LEN] = { 0U };
+    char time_buffer[12] = { 0U }; // Buffer for timestamp in format [hh:mm:ss]
+
+    size_t preamble_len = strlen(log_config->level_str);
+    size_t func_len = strlen(func);
+    size_t msg_len = strlen(msg);
+
+    size_t complete_msg_len = preamble_len + func_len + 2 + msg_len + 1; // +2 for ": " and +1 for null terminator
+
+    if (complete_msg_len > LOG_PRINT_BUFFER_LEN) {
+        return COOL_OVERFLOW;
+    }
+
+    // Assemble the message with time buffer
+    int snprintf_result = snprintf(buffer, sizeof(buffer), "%s%s%s: %s", log_config->level_str, time_buffer, func, msg);
+    if (snprintf_result < 0 || (size_t)snprintf_result >= sizeof(buffer)) {
+        return COOL_FORMAT_ERROR;
+    }
+
+    int vprintf_result = vprintf(buffer, args);
+    if (vprintf_result < 0 || (size_t)vprintf_result != strlen(buffer)) {
+        return COOL_PRINT_FAILED;
+    }
+
+    return COOL_OK;
+}
+
+static cool_status_t log_generic(log_level_list_t level, const char * restrict func, const char * restrict msg, va_list args)
+{
+    cool_status_t result = print_log_msg(&log_level_and_string[level], func, msg, args);
+    return result;
+}
 
 cool_status_t logger_init(log_level_list_t log_level)
 {
@@ -186,43 +222,4 @@ cool_status_t log_trace(const char * restrict func, const char * restrict msg, .
     cool_status_t result = log_generic(TRACE, func, msg, args);
     va_end(args);
     return result;
-}
-
-static cool_status_t log_generic(log_level_list_t level, const char * restrict func, const char * restrict msg, va_list args)
-{
-    cool_status_t result = print_log_msg(&log_level_and_string[level], func, msg, args);
-    return result;
-}
-
-static cool_status_t print_log_msg(const log_level_and_string_t *log_config, const char * restrict func, const char * restrict msg, va_list args)
-{
-    if (log_config == NULL || logger_conf.level < log_config->level) {
-        return COOL_NULL_POINTER_ERROR;
-    }
-
-    char buffer[LOG_PRINT_BUFFER_LEN] = { 0U };
-    char time_buffer[12] = { 0U }; // Buffer for timestamp in format [hh:mm:ss]
-
-    size_t preamble_len = strlen(log_config->level_str);
-    size_t func_len = strlen(func);
-    size_t msg_len = strlen(msg);
-
-    size_t complete_msg_len = preamble_len + func_len + 2 + msg_len + 1; // +2 for ": " and +1 for null terminator
-
-    if (complete_msg_len > LOG_PRINT_BUFFER_LEN) {
-        return COOL_OVERFLOW;
-    }
-
-    // Assemble the message with time buffer
-    int snprintf_result = snprintf(buffer, sizeof(buffer), "%s%s%s: %s", log_config->level_str, time_buffer, func, msg);
-    if (snprintf_result < 0 || (size_t)snprintf_result >= sizeof(buffer)) {
-        return COOL_FORMAT_ERROR;
-    }
-
-    int vprintf_result = vprintf(buffer, args);
-    if (vprintf_result < 0 || (size_t)vprintf_result != strlen(buffer)) {
-        return COOL_PRINT_FAILED;
-    }
-
-    return COOL_OK;
 }
