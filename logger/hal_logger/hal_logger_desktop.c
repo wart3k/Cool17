2a9ab8e83b165f6cf88e0e75d4583167ce5a82b7
Thomas Woltering <wart3k@web.de>
2025-01-11 01:43:47 +0100

    removed hal_log + timer

--- logger/hal_logger/hal_logger_desktop.c
+++
@@ -1,9 +0,0 @@
-//
-// Created by WART3K on 20.10.24.
-//
-
-// #include "hal_logger_desktop.h"
-//
-// #if defined(__APPLE__) || defined(_WIN64) || defined(__linux__)
-// #define TEST_MACRO
-// #endif //defined(__APPLE__) || defined(_WIN64) || defined(__linux__)
--- logger/logger.c
+++ logger/logger.c
@@ -4,21 +4,16 @@
 
 #include "logger.h"
 
-#include <stdbool.h>
 #include <stdint.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <string.h>
 
-#include "hal_logger/hal_logger.h"
-
-#define LOG_LEVEL_OFFSET        1
-#define LOG_LEVEL_COUNT         (5 + LOG_LEVEL_OFFSET)
-#define LOG_PRINT_BUFFER_LEN    UINT8_MAX
+#define LOG_LEVEL_COUNT         6U
+#define LOG_PRINT_BUFFER_LEN    UINT16_MAX
 
 typedef struct {
     log_level_list_t level;
-    bool is_timer_available;
 }logger_config_t;
 
 typedef struct {
@@ -34,8 +29,7 @@ static const char LOG_DEBUG_CHAR[]      = "[DEBUG   ]: ";
 static const char LOG_TRACE_CHAR[]      = "[TRACE   ]: ";
 
 static logger_config_t logger_conf = {
-    .level = INFO,
-    .is_timer_available = false
+    .level = INFO
 };
 
 static const log_level_and_string_t log_level_and_string[LOG_LEVEL_COUNT] = {
@@ -47,66 +41,54 @@ static const log_level_and_string_t log_level_and_string[LOG_LEVEL_COUNT] = {
     { .level = TRACE, .level_str = LOG_TRACE_CHAR }
 };
 
-static cool_status_t print_log_msg(const log_level_and_string_t *log_config, const char * restrict func, const char * restrict msg, va_list args)
+static cool_status_t print_log_msg(const log_level_and_string_t *log_config,
+                                   const char * restrict func,
+                                   const char * restrict msg,
+                                   const va_list args)
 {
-    if (log_config == NULL || logger_conf.level < log_config->level) {
+    if (log_config == NULL || logger_conf.level < log_config->level)
+    {
         return COOL_NULL_POINTER_ERROR;
     }
 
     char buffer[LOG_PRINT_BUFFER_LEN] = { 0U };
 
-    size_t preamble_len = strlen(log_config->level_str);
-    size_t func_len = strlen(func);
-    size_t msg_len = strlen(msg);
+    const size_t preamble_len = strlen(log_config->level_str);
+    const size_t func_len = strlen(func);
+    const size_t msg_len = strlen(msg);
 
-    if (hal_logger_init() != COOL_OK) {
-        size_t complete_msg_len = preamble_len + func_len + 2 + msg_len + 1; // +2 for ": " and +1 for null terminator
+    const size_t complete_msg_len = preamble_len + func_len + 2 + msg_len + 1; // +2 for ": " and +1 for null terminator
 
-        if (complete_msg_len > LOG_PRINT_BUFFER_LEN) {
-            return COOL_OVERFLOW;
-        }
-
-        int snprintf_result = snprintf(buffer, sizeof(buffer), "%s%s: %s", log_config->level_str, func, msg);
-        if (snprintf_result < 0 || (size_t)snprintf_result >= sizeof(buffer)) {
-            return COOL_FORMAT_ERROR;
-        }
-
-        int vprintf_result = vprintf(buffer, args);
-        if (vprintf_result < 0 || (size_t)vprintf_result != strlen(buffer)) {
-            return COOL_PRINT_FAILED;
-        }
-
-        return COOL_OK;
-    }
-
-    size_t complete_msg_len = preamble_len + func_len + 2 + msg_len + 1; // +2 for ": " and +1 for null terminator
-    char time_buffer[12] = { 0U }; // Buffer for timestamp in format [hh:mm:ss]
-
-    if (complete_msg_len > LOG_PRINT_BUFFER_LEN) {
+    if (complete_msg_len > LOG_PRINT_BUFFER_LEN)
+    {
         return COOL_OVERFLOW;
     }
 
-    // Assemble the message with time buffer
-    int snprintf_result = snprintf(buffer, sizeof(buffer), "%s%s%s: %s", log_config->level_str, time_buffer, func, msg);
-    if (snprintf_result < 0 || (size_t)snprintf_result >= sizeof(buffer)) {
+    const int snprintf_result = snprintf(buffer, sizeof(buffer), "%s%s: %s", log_config->level_str, func, msg);
+    if (snprintf_result < 0 || (size_t)snprintf_result >= sizeof(buffer))
+    {
         return COOL_FORMAT_ERROR;
     }
 
-    int vprintf_result = vprintf(buffer, args);
-    if (vprintf_result < 0 || (size_t)vprintf_result != strlen(buffer)) {
+    const int vprintf_result = vprintf(buffer, args);
+    if (vprintf_result < 0 || (size_t)vprintf_result != strlen(buffer))
+    {
         return COOL_PRINT_FAILED;
     }
 
     return COOL_OK;
 }
 
-static cool_status_t log_generic(log_level_list_t level, const char * restrict func, const char * restrict msg, va_list args)
+static cool_status_t log_generic(const log_level_list_t level,
+                                 const char * restrict func,
+                                 const char * restrict msg,
+                                 const va_list args)
 {
-    cool_status_t result = print_log_msg(&log_level_and_string[level], func, msg, args);
+    const cool_status_t result = print_log_msg(&log_level_and_string[level], func, msg, args);
     return result;
 }
 
-cool_status_t logger_init(log_level_list_t log_level)
+cool_status_t logger_init(const log_level_list_t log_level)
 {
     if (log_level > TRACE)
     {
@@ -116,7 +98,7 @@ cool_status_t logger_init(log_level_list_t log_level)
     return COOL_OK;
 }
 
-cool_status_t logger_set_level(log_level_list_t log_level)
+cool_status_t logger_set_level(const log_level_list_t log_level)
 {
     if(log_level > TRACE)
     {
@@ -139,14 +121,14 @@ cool_status_t log_error(const char * restrict func, const char * restrict msg, .
         return COOL_WRONG_INPUT_PARAMETER;
     }
 
-    if((logger_conf.level + LOG_LEVEL_OFFSET) <= ERROR)
+    if((logger_conf.level) < ERROR)
     {
         return COOL_OK;
     }
 
     va_list args;
     va_start(args, msg);
-    cool_status_t result = log_generic(ERROR, func, msg, args);
+    const cool_status_t result = log_generic(ERROR, func, msg, args);
     va_end(args);
     return result;
 }
@@ -158,14 +140,14 @@ cool_status_t log_critical(const char * restrict func, const char * restrict msg
         return COOL_WRONG_INPUT_PARAMETER;
     }
 
-    if((logger_conf.level + LOG_LEVEL_OFFSET) <= CRITICAL)
+    if((logger_conf.level) < CRITICAL)
     {
         return COOL_OK;
     }
 
     va_list args;
     va_start(args, msg);
-    cool_status_t result = log_generic(CRITICAL, func, msg, args);
+    const cool_status_t result = log_generic(CRITICAL, func, msg, args);
     va_end(args);
     return result;
 }
@@ -177,14 +159,14 @@ cool_status_t log_warning(const char * restrict func, const char * restrict msg,
         return COOL_WRONG_INPUT_PARAMETER;
     }
 
-    if((logger_conf.level + LOG_LEVEL_OFFSET) <= WARNING)
+    if((logger_conf.level) < WARNING)
     {
         return COOL_OK;
     }
 
     va_list args;
     va_start(args, msg);
-    cool_status_t result = log_generic(WARNING, func, msg, args);
+    const cool_status_t result = log_generic(WARNING, func, msg, args);
     va_end(args);
     return result;
 }
@@ -196,14 +178,14 @@ cool_status_t log_info(const char * restrict func, const char * restrict msg, ..
         return COOL_WRONG_INPUT_PARAMETER;
     }
 
-    if((logger_conf.level + LOG_LEVEL_OFFSET) <= INFO)
+    if((logger_conf.level) < INFO)
     {
         return COOL_OK;
     }
 
     va_list args;
     va_start(args, msg);
-    cool_status_t result = log_generic(INFO, func, msg, args);
+    const cool_status_t result = log_generic(INFO, func, msg, args);
     va_end(args);
     return result;
 }
@@ -215,14 +197,14 @@ cool_status_t log_debug(const char * restrict func, const char * restrict msg, .
         return COOL_WRONG_INPUT_PARAMETER;
     }
 
-    if((logger_conf.level + LOG_LEVEL_OFFSET) <= DEBUG)
+    if((logger_conf.level) < DEBUG)
     {
         return COOL_OK;
     }
 
     va_list args;
     va_start(args, msg);
-    cool_status_t result = log_generic(DEBUG, func, msg, args);
+    const cool_status_t result = log_generic(DEBUG, func, msg, args);
     va_end(args);
     return result;
 }
@@ -234,14 +216,14 @@ cool_status_t log_trace(const char * restrict func, const char * restrict msg, .
         return COOL_WRONG_INPUT_PARAMETER;
     }
 
-    if((logger_conf.level + LOG_LEVEL_OFFSET) <= TRACE)
+    if((logger_conf.level) < TRACE)
     {
         return COOL_OK;
     }
 
     va_list args;
     va_start(args, msg);
-    cool_status_t result = log_generic(TRACE, func, msg, args);
+    const cool_status_t result = log_generic(TRACE, func, msg, args);
     va_end(args);
     return result;
 }
